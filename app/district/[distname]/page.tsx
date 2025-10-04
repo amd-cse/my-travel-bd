037c5d77c2a79b8c829cb262b27464abcd9a3e11
amd-cse <ahmed.mustafa4590@gmail.com>
2025-10-04 14:18:31 +0600

    added new district

--- app/district/[distname]/page.tsx
+++ app/district/[distname]/page.tsx
@@ -3,9 +3,8 @@ import { notFound } from 'next/navigation';
 import { FeatureCollection, Point } from 'geojson';
 import { getLocationOrigin } from "next/dist/shared/lib/utils";
 
-const DIST = ['Dhaka', "Cox's_Bazar"];
-// Adding a comment
-//Another comment
+const DIST = ['Dhaka', "Cox's_Bazar", "Chittagong"];
+
 export default async function DistPage({ params }: { params: { distname: string } }) {
     const dName = await params;
     const distName = decodeURIComponent(dName.distname);
